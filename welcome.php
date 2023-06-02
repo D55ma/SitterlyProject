4264e09f772940b27081c49fa07913c921633b92
D55ma <100362815+D55ma@users.noreply.github.com>
2023-06-03 01:58:04 +0300

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Sitterly</title>
+<link rel="stylesheet" type="text/css" href="c1.css">
+</head>
+
+<body>
+<div class="container" onclick="onclick">
+  <div class="top"></div>
+  <div class="bottom"></div>
+  <div class="center">
+ 
+
+<form action="#"  class = " scrollbar" id="style-1" >
+
+<img  id="logo" src="img/logo-circle.png" width="100px" height="100px"  >
+<p>Unlike other babysitting websites, services, or nanny agencies, Sitterly provides you with baby sitter reviews written by other parents so we gain your trust.</p>
+  <a href="signupparent.php"><button class="CancelB"  type ="button"display: flex; " >I'm a Parent</button></a>
+	<a href="signupsitter.php"><button type="button" class="CancelB"  style="display: flex; background:rgb(210, 182, 205); " >I'm a sitter</button></a>
+<a href="signin.php"><button type="button" class="CancelB"  style="display: flex; background:#F5F5DD; ">Sign in</button></a>
+	 
+<footer>
+  <p class="mini">Author: Sitterly Team
+  <a href="mailto:Sitterly@gmail.com">Contact us<strong>&copy;</strong></a></p>
+</footer>
+
+</form>
+
+
+</div>
+</div>
+	</body>
+</html>
